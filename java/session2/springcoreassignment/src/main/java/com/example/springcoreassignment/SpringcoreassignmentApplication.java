3931e2b01044512eb50eb0a879a1a0fc129b1c0e
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 13:24:56 +0530

    feat: project initialize

---
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/SpringcoreassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoreassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreassignmentApplication.class, args);
+	}
+
+}
---
+++ java/session2/springcoreassignment/src/test/java/com/example/springcoreassignment/SpringcoreassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoreassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
