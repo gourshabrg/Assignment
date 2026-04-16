a82f0a2638f242bb4bf9429370df04c4c0e9c681
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 15:06:30 +0530

    feat: add the notification service

--- java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/service/NotificationService.java
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/service/NotificationService.java
@@ -1,5 +1,21 @@
 package com.example.springcoreassignment.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.springcoreassignment.component.NotificationComponent;
+
+@Service
 public class NotificationService {
-  
+
+    private final NotificationComponent notificationComponent;
+
+    // constructor-based dependency injection for NotificationComponent
+    public NotificationService(NotificationComponent notificationComponent) {
+        this.notificationComponent = notificationComponent;
+    }
+
+    // method to trigger notification
+    public String triggerNotification() {
+        return notificationComponent.sendNotification();
+    }
 }
