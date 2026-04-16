bae6fecc6fa66f1f07e0aa1a548081430b086c89
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 14:35:01 +0530

    feat: added duplicate user exception logic

--- java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/exception/DuplicateUserException.java
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/exception/DuplicateUserException.java
@@ -1,5 +1,9 @@
 package com.example.springcoreassignment.exception;
 
-public class DuplicateUserException {
-  
+public class DuplicateUserException extends RuntimeException {
+
+    // constructor to initialize the exception message
+    public DuplicateUserException(String message) {
+        super(message);
+    }
 }
