67b418873293c211aa80bfd553a3049f2761325c
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 23:51:59 +0530

    feat: inside model user model logic written

--- java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/model/User.java
+++ java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/model/User.java
@@ -1,5 +1,21 @@
 package com.ravindra.springrestassignment.model;
 
 public class User {
-  
+
+    private int id;
+    private String name;
+    private int age;
+    private String role;
+
+    public User(int id, String name, int age, String role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getRole() { return role; }
 }
