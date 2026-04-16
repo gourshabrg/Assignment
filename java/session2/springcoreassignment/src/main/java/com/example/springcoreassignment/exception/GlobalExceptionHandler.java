65eb67a120a8f27f20a7f421f718c7816f203f84
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 14:39:09 +0530

    feat: add the global exception

--- java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/exception/GlobalExceptionHandler.java
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/exception/GlobalExceptionHandler.java
@@ -1,5 +1,41 @@
 package com.example.springcoreassignment.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 
 public class GlobalExceptionHandler {
-  
+  // Handle UserNotFoundException
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Handle DuplicateUserException
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<String> handleDuplicateUser(DuplicateUserException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    // Handle any other exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>(
+                "Something went wrong: " + ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Handle validation exceptions
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+
+        String error = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return ResponseEntity.badRequest().body(error);
+    }
 }
