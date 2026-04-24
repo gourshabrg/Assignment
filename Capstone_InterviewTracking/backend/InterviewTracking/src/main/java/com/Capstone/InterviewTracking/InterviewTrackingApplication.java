9c570968ef3c627ea4afd73483544e37dd16457b
Gourshab <ravindragourcode786@gmail.com>
2026-04-24 18:45:43 +0530

    by mistake change the itnerveiw traking application file

--- Capstone_InterviewTracking/backend/InterviewTracking/src/main/java/com/Capstone/InterviewTracking/InterviewTrackingApplication.java
+++ Capstone_InterviewTracking/backend/InterviewTracking/src/main/java/com/Capstone/InterviewTracking/InterviewTrackingApplication.java
@@ -1,7 +1,13 @@
-package com.Capstone.InterviewTracking.enums;
+package com.Capstone.InterviewTracking;
 
-public enum RoleType {
-	HR,
-	PANEL,
-	CANDIDATE
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTrackingApplication.class, args);
+	}
+
 }
