40579446f88e202a22a3584a728c1e372ce919b6
Gourshab <ravindragourcode786@gmail.com>
2026-06-12 14:10:34 +0530

    feat: added the program to check python version

---
+++ Ravindra__python_training/Assignment_1/introduction/python_version.py
@@ -0,0 +1,9 @@
+import sys # Importing sys module to access python version information
+
+# Function to display python version
+
+def show_python_version():
+    print("Python Version:", sys.version)
+
+# Calling function
+show_python_version()
