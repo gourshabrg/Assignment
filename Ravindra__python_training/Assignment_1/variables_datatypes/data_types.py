c40374a32766a8b4d56f506bc0655e77434f0771
Gourshab <ravindragourcode786@gmail.com>
2026-06-12 14:20:28 +0530

    feat: added the data type program

---
+++ Ravindra__python_training/Assignment_1/variables_datatypes/data_types.py
@@ -0,0 +1,15 @@
+# 4. Create variables of type int, float, string, and boolean. Print their types using type().
+
+def show_data_types():
+    number = 10
+    decimal = 10.5
+    text = "Python"
+    status = True
+
+    print(type(number))
+    print(type(decimal))
+    print(type(text))
+    print(type(status))
+
+
+show_data_types()
