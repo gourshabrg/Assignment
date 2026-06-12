9f4703352c2e69a872a5e00ab960075174e71f68
Gourshab <ravindragourcode786@gmail.com>
2026-06-12 16:10:09 +0530

    feat: added the program calculate the square

---
+++ Ravindra__python_training/Assignment_1/Functions/square_function.py
@@ -0,0 +1,13 @@
+# 17. Write a function to calculate square of a number.
+
+
+def calculate_square(number):
+
+    square = number * number
+
+    print(f"Square ={square}")
+
+
+number = float(input("Enter a number: "))
+
+calculate_square(number)
