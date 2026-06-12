1ba8b8dba4d57bf24400e05ff0ccd88d9a36c36b
Gourshab <ravindragourcode786@gmail.com>
2026-06-12 16:34:54 +0530

    feat: added the program of random number

---
+++ Ravindra__python_training/Assignment_1/Modules/random_module_demo.py
@@ -0,0 +1,11 @@
+# 23. Generate random numbers using random module.
+
+import random
+
+def generate_random_numbers():
+
+    print("Random Integer =", random.randint(1, 100))
+    print("Random Float =", random.random())
+
+
+generate_random_numbers()
