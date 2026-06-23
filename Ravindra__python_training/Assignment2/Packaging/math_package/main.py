ef0f7cd419751949916aea89f4fcb9dba1beb9d3
Gourshab <ravindragourcode786@gmail.com>
2026-06-23 15:00:14 +0530

    feat: create reusable math operations package

---
+++ Ravindra__python_training/Assignment2/Packaging/math_package/main.py
@@ -0,0 +1,15 @@
+from math_operations.add import add
+
+from math_operations.subtract import subtract
+
+from math_operations.multiply import multiply
+
+from math_operations.divide import divide
+
+print(f"Addition = {add(10, 5)}")
+
+print(f"Subtraction = {subtract(10, 5)}")
+
+print(f"Multiplication = {multiply(10, 5)}")
+
+print(f"Division = {divide(10, 5)}")
