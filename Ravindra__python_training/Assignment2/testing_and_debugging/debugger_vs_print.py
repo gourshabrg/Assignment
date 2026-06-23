28f5642b93989801dfc6fde3588fc5ddfef6a1a7
Gourshab <ravindragourcode786@gmail.com>
2026-06-23 14:26:23 +0530

    feat: explain debugger advantages

---
+++ Ravindra__python_training/Assignment2/testing_and_debugging/debugger_vs_print.py
@@ -0,0 +1,14 @@
+# Explain the advantages of using an IDE debugger over print statements.
+
+
+# 1. Pause execution at any line.
+
+# 2. View variable values directly.
+
+# 3. Step through code line by line.
+
+# 4. Easier to find logical bugs.
+
+# 5. No need to add multiple print statements.
+
+# 6. Faster debugging for large projects.
