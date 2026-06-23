e98eb7ac0a8a02055367e70b08dde955902515a0
Gourshab <ravindragourcode786@gmail.com>
2026-06-23 12:59:25 +0530

    feat: add lambda function example

---
+++ Ravindra__python_training/Assignment2/functional_programming/lambda_square.py
@@ -0,0 +1,8 @@
+#  Write a lambda function to find the square of a number.
+
+
+square = lambda number: number * number
+
+number = int(input("Enter a number: "))
+
+print(f"Square = {square(number)}")
